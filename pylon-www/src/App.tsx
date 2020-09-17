dfe169575c7e1030553ca2f7eb6e6e0501f4fdd4
dezzzus <alex_dorn@mail.com>
2020-09-17 11:54:00 -0400

    fix issue

--- pylon-www/src/App.tsx
+++ pylon-www/src/App.tsx
@@ -47,7 +47,7 @@ const App: React.FC = () => {
             }}
           /> */}
           <Route path="/" exact>
-            <Home />
+            <Farms />
           </Route>
           <Route path="/farms">
             <Farms />
